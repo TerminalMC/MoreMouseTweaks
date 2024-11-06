1eb08181cd10eb458be6ce417b78048b6e3fe4e0
NotRyken <127091011+NotRyken@users.noreply.github.com>
2024-11-06 14:30:57 +0800

    Fix a crash when quick-crafting from alternative recipe slots

--- common/src/main/java/dev/terminalmc/moremousetweaks/mixin/gui/other/MixinOverlayRecipeComponent.java
+++ common/src/main/java/dev/terminalmc/moremousetweaks/mixin/gui/other/MixinOverlayRecipeComponent.java
@@ -52,8 +52,8 @@ public class MixinOverlayRecipeComponent {
             do {
                 if (!iter.hasNext()) {
                     cir.setReturnValue(false);
+                    return; // Crash prevention
                 }
-
                 overlayButton = iter.next();
             } while(!overlayButton.mouseClicked(mouseX, mouseY, 0));
 
